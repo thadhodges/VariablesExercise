6d83ac48212d9347c8abb227b6f689679430bd03
Thad <thadisfree@gmail.com>
2021-07-02 21:51:49 -0500

    recreated project file to eliminate error

---
+++ VariablesExercise/VariablesExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John Smith";
+            int age = 46;
+            char letter = 'R';
+            bool isMarried = true;
+            double kids = 5.0;
+            decimal yearsWorked = 25;
+
+            Console.WriteLine($"Hello World, {name}!/n" +
+                $"I see from your profile that you are married with/n" +
+                $"{kids} children.");
+        }
+    }
+}
