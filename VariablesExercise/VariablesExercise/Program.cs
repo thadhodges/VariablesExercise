f9754eeac5cc2903b47c060c3e6920a0f8bb863d
Thad <thadisfree@gmail.com>
2021-07-02 22:00:09 -0500

    build narrative

--- VariablesExercise/VariablesExercise/Program.cs
+++ VariablesExercise/VariablesExercise/Program.cs
@@ -13,9 +13,10 @@ namespace VariablesExercise
             double kids = 5.0;
             decimal yearsWorked = 25;
 
-            Console.WriteLine($"Hello World, {name}!/n" +
-                $"I see from your profile that you are married with/n" +
-                $"{kids} children.");
+            Console.WriteLine($"Hello {name}!");
+            Console.WriteLine($"I see from your profile that you have {kids} children;");
+            Console.WriteLine($"are married, and have been employed for {yearsWorked} years.");
+            Console.WriteLine($"It also says you are {age} years onld and your favorite letter is {letter}.");
         }
     }
 }
