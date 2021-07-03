eb515c045a25aa29666f8ca4eb6d9832798308a5
Thad <thadisfree@gmail.com>
2021-07-02 22:17:09 -0500

    commented out unneed code

--- VariablesExercise/VariablesExercise/Program.cs
+++ VariablesExercise/VariablesExercise/Program.cs
@@ -8,15 +8,17 @@ namespace VariablesExercise
         {
             string name = "John Smith";
             int age = 46;
+            //int kidsPerYearsWorked;
             char letter = 'R';
             bool isMarried = true;
             double kids = 5.0;
             decimal yearsWorked = 25;
-
+            //kidsPerYearsWorked = age * age;
             Console.WriteLine($"Hello {name}!");
             Console.WriteLine($"I see from your profile that you have {kids} children;");
             Console.WriteLine($"are married, and have been employed for {yearsWorked} years.");
             Console.WriteLine($"It also says you are {age} years onld and your favorite letter is {letter}.");
+            //Console.WriteLine(kidsPerYearsWorked);
         }
     }
 }
